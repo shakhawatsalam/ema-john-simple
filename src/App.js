26ee0516a7f35bffdc618b3fe7b9b3275e18b92f
skshawon666 <75742736+skshawon666@users.noreply.github.com>
2022-03-30 23:39:09 +0600

    hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
 import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
 import Orders from './components/Orders/Orders';
@@ -10,10 +11,11 @@ function App() {
     <div>
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Shop></Shop>} />
+        <Route path='/' element={<Shop />} />
         <Route path='/shop' element={<Shop />} />
         <Route path='/orders' element={<Orders />} />
         <Route path='/inventory' element={<Inventory />} />
+        <Route path='/about' element={<About/>} />
       </Routes>
     </div>
   );
