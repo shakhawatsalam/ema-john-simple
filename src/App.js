756444a1ae70355961e2ed34f5cffab8546dce65
skshawon666 <75742736+skshawon666@users.noreply.github.com>
2022-03-30 23:11:53 +0600

    install reat router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import Orders from './components/Orders/Orders';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+        <Route path='/' element={<Shop></Shop>} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/inventory' element={<Inventory />} />
+      </Routes>
     </div>
   );
 }
